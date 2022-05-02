9444271e59c7a2697c505a1da022282ce2d06368
David Chen <david.chen@liveramp.com>
2022-05-02 23:13:30 +0800

    feat: change mod name

--- cmd/server/wire_gen.go
+++ cmd/server/wire_gen.go
@@ -6,11 +6,11 @@
 package main
 
 import (
-	"github.com/go-kratos/kratos-layout/internal/biz"
-	"github.com/go-kratos/kratos-layout/internal/conf"
-	"github.com/go-kratos/kratos-layout/internal/data"
-	"github.com/go-kratos/kratos-layout/internal/server"
-	"github.com/go-kratos/kratos-layout/internal/service"
+	"github.com/davidchen-cn/go-layout/internal/biz"
+	"github.com/davidchen-cn/go-layout/internal/conf"
+	"github.com/davidchen-cn/go-layout/internal/data"
+	"github.com/davidchen-cn/go-layout/internal/server"
+	"github.com/davidchen-cn/go-layout/internal/service"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
 )
--- internal/data/data.go
+++ internal/data/data.go
@@ -1,7 +1,7 @@
 package data
 
 import (
-	"github.com/go-kratos/kratos-layout/internal/conf"
+	"github.com/davidchen-cn/go-layout/internal/conf"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
 )
